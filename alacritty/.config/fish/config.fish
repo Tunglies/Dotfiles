e07e81f973006ce6a759b85b24d23a62f8a24666
Tunglies <tunglies.dev@outlook.com>
2025-05-21 02:10:55 +0800

    Update Alacritty configuration with new padding, opacity, and decorations; enhance Fish prompt with greeting and path adjustments; add done.fish script for command completion notifications.

--- alacritty/.config/fish/config.fish
+++ alacritty/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -U fish_greeting "Hello Tunglies!
+    (=^･ω･^=)"
+
+    set -gx PATH /opt/homebrew/bin $PATH
 end
