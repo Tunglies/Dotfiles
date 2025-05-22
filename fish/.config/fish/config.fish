7f6ed20b94c6e79c0eb69531eb02f89b47e4eb2e
Tunglies <tunglies.dev@outlook.com>
2025-05-22 14:21:17 +0800

    Add proxy configuration function in Fish settings and invoke it in interactive sessions.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,14 @@
+function set_proxy
+    set -x http_proxy http://127.0.0.1:7897
+    set -x https_proxy http://127.0.0.1:7897
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting "Hello Tunglies!
     (=^･ω･^=)"
 
+    set_proxy
+    
     set -gx PATH /opt/homebrew/bin $PATH
 end
