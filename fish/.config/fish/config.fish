bf2d64720b4842a6272b10ffef95abd5f496ae65
Tunglies <tunglies.dev@outlook.com>
2025-06-05 23:31:21 +0800

    Implement code changes to enhance functionality and improve performance

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,5 +13,25 @@ if status is-interactive
     set -gx PATH /opt/homebrew/bin $PATH
     set -gx PATH ~/.cargo/bin $PATH
 
+    alias gcl="git clone"
+    alias gpl="git pull --rebase"
+    alias gp="git push"
+    alias gcm="git commit -m"
+    alias gc="git commit"
+    alias gco="git checkout"
+    alias gbr="git branch"
+    alias gm="git merge --rebase"
+    alias gcp="git cherry-pick"
+    alias gst="git status"
+    alias gds="git diff --staged"
+    alias gdc="git diff"
+
     zoxide init fish | source
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/tunglies/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
